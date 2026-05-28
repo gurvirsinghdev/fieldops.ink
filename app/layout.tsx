48b2e42c092a3471abd0ad7018160ce5b674b768
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-05-28 14:37:46 -0700

    feat: shadcn installation and theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Antic, Noto_Sans_Georgian, JetBrains_Mono } from "next/font/google";
+
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const mono = JetBrains_Mono();
+const serif = Noto_Sans_Georgian();
+const sans = Antic({ weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full antialiased",
+        sans.className,
+        serif.className,
+        mono.className,
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
